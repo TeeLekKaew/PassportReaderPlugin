1782aebdb8dd08ec7ac5ed385fbb480005455334
TeeLeK Kaew <>
2020-01-23 10:23:04 +0700

    add passportReader fnc.

--- src/android/PassportReader.java
+++ src/android/PassportReader.java
@@ -2,6 +2,10 @@ package th.co.bbtec.cordova.plugin;
 
 import com.google.gson.Gson;
 
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
@@ -19,6 +23,18 @@ public class PassportReader extends CordovaPlugin {
 
             this.coolMethod(dog, callbackContext);
             return true;
+        }else if (action.equals("readPassport")) {
+
+            JSONObject obj = args.getJSONObject(0);// .getString(0);
+            PassportData passportData = new PassportData(
+                obj.getString("id"),
+                obj.getString("passportNumber"),
+                obj.getString("expirationDate"),
+                obj.getString("birthDate"));
+
+
+            this.readPassport(passportData, callbackContext);
+            return true;
         }
         return false;
     }
@@ -34,8 +50,98 @@ public class PassportReader extends CordovaPlugin {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+    private void readPassport(PassportData passportData, CallbackContext callbackContext) {
+        if (passportData != null) {
+            Tag tag = passportData.getTag();
+
+            if (Arrays.asList(tag.getTechList()).contains("android.nfc.tech.IsoDep")) {
+                // SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                // String passportNumber = preferences.getString(KEY_PASSPORT_NUMBER, null);
+                // String expirationDate = convertDate(preferences.getString(KEY_EXPIRATION_DATE, null));
+                // String birthDate = convertDate(preferences.getString(KEY_BIRTH_DATE, null));
+                if (passportData.getPassportNumber() != null && !passportData.getPassportNumber().isEmpty()
+                        && passportData.getExpirationDate() != null && !passportData.getExpirationDate().isEmpty()
+                        && passportData.getBirthDate() != null && !passportData.getBirthDate().isEmpty()) {
+            
+                    BACKeySpec bacKey = new BACKey(passportNumber, birthDate, expirationDate);
+                    // new ReadTask(IsoDep.get(tag), bacKey).execute();
+                    // mainLayout.setVisibility(View.GONE);
+                    // loadingLayout.setVisibility(View.VISIBLE);
+                    callbackContext.success("Passport No. : " + passportData.getPassportNumber());
+                } else {
+                    callbackContext.error("Please provide details to read passport");
+                    // Snackbar.make(passportNumberView, R.string.error_input, Snackbar.LENGTH_SHORT).show();
+                }
+            }else{
+                Log.e(TAG, "onNewIntent > NOT contain android.nfc.tech.IsoDep");
+                callbackContext.error("NOT contain android.nfc.tech.IsoDep");
+            }
+
+            
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+
 }
 
+class PassportData {
+    private Tag tag;
+    private String id;
+    private String passportNumber;
+    private String expirationDate;
+    private String birthDate;
+
+    public PassportData(String id, String passportNumber, String expirationDate, String birthDate) {
+        this.id = id;
+        this.passportNumber = passportNumber;
+        this.expirationDate = expirationDate;
+        this.birthDate = birthDate;
+    }
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+}
 class Dog {
     private String name;
     private Integer age;
@@ -61,3 +167,4 @@ class Dog {
         this.age = age;
     }
 }
+
--- www/PassportReader.js
+++ www/PassportReader.js
@@ -7,6 +7,10 @@ PassportReader.prototype.coolMethod = function (arg0, success, error) {
     exec(success, error, 'PassportReader', 'coolMethod', [arg0]);
 };
 
+PassportReader.prototype.readPassport = function (arg0, success, error) {
+    exec(success, error, 'PassportReader', 'readPassport', [arg0]);
+};
+
 if (typeof module != 'undefined' && module.exports) {
     module.exports = PassportReader;
 }
